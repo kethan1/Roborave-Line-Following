092a92a9557aa9980a0786a1cb4d98a63b7cb2b2
Kethan Vegunta <kethan@vegunta.com>
2021-05-17 17:59:24 -0700

    update code

--- thunderborg_testing.py
+++ thunderborg_testing.py
@@ -1,3 +1,7 @@
+# GPIO 25 - Encoder A for Motor1
+# GPIO 22 - Encoder B for Motor1
+# GPIO 24 - Encoder A for Motor2
+# GPIO 23 - Encoder B for Motor2
 import Libraries.ThunderBorg3 as ThunderBorg
 import sys
 import time
@@ -17,41 +21,13 @@ if not TB.foundChip:
         print('TB.i2cAddress = 0x%02X' % (boards[0]))
     sys.exit()
 
-# TB.SetLedShowBattery(False)
 TB.SetBatteryMonitoringLimits(7.5, 9)
 
-# TB.SetLed1(0, 0, 255)
-# time.sleep(0.2)
-# TB.SetLed1(0, 0, 0)
-# time.sleep(0.2)
-
-# TB.SetLed1(0, 0, 255)
-# time.sleep(0.2)
-# TB.SetLed1(0, 0, 0)
-# time.sleep(0.2)
-
-# TB.SetLed1(0, 255, 0)
-
-# TB.SetMotor1(1)
-# time.sleep(2)
-# TB.SetMotor1(0)
-from Encoder import Encoder
-encoder = Encoder(23)
 try:
-    TB.SetMotor2(1)
-    time.sleep(30)
+    TB.SetMotor1(1)
+    while True:
+        time.sleep(1)
+    # TB.SetMotor1(1)
 except:
     pass
 TB.MotorsOff()
-
-# TB.SetLed1(255, 255, 0)
-# time.sleep(0.2)
-# TB.SetLed1(0, 0, 0)
-# time.sleep(0.2)
-
-# TB.SetLed1(255, 255, 0)
-# time.sleep(0.2)
-# TB.SetLed1(0, 0, 0)
-# time.sleep(0.2)
-
-# TB.SetLedShowBattery(True)
